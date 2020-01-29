3bbedc7a99e7ed984cd53f8dac7380efafdb218a
Lynette Nguyen <lynettenguyen0909@gmail.com>
2020-01-28 23:33:40 -0800

    "Added Part 2"

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Lynette Nguyen on 1/28/20.
+//  Copyright © 2020 Lynette Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+}
+
